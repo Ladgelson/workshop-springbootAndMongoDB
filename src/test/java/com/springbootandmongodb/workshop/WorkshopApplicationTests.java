6661a9b590bb4aa4ac5bb249b99cb2001b9c8497
Ladgelson <micaiasladgelsondasilva@gmail.com>
2021-09-10 16:53:21 -0300

    first commit

---
+++ src/test/java/com/springbootandmongodb/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootandmongodb.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
